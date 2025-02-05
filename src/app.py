d4814039989d9512cbc97bc74ffa7be3ca1b6821
Aaron Campos <aaroncm611@gmail.com>
2025-02-05 21:23:24 +0100

    añadimos vista de register y login

--- src/app.py
+++ src/app.py
@@ -1,15 +1,29 @@
 from flask import Flask, render_template, redirect, url_for, request
-
+from flask_mysqldb import MySQL
+from config import config
 
 app = Flask(__name__)
 
+mysql = MySQL(app)
+
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    cur.execute('select * from products')
+    products  = cur.fetchall()
+    print(products)
+    return render_template('index.html', products=products)
+
+@app.route('/agregar_fav/<string:id>')
+def agregar_favoritos(id):
+    if id == '':
+        print(f"Producto con ID {id} agregado a favoritos")
+    return redirect(url_for('login'))
+
 
 @app.route('/login',methods=['GET','POST'])
 def login():
-    pass
+    return render_template('login.html')
 
 @app.route('/register', methods=['GET','POST'])
 def register():
@@ -20,4 +34,5 @@ def users():
     pass
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.config.from_object(config['development'])
+    app.run()
---
+++ src/db/products.sql
@@ -0,0 +1,6 @@
+use flask_practis_sql
+
+INSERT INTO products VALUES 
+(NULL,'Monitor',105,'https://misura.s11.cdn-upgates.com/_cache/c/1/c15cd65eecf37eef2e0f6214a1658d3e-eg34rwa-foto05.jpg'),
+(NULL,'Raton',50,'https://www.info-computer.com/306304-large_default/raton-glorious-pc-gaming-race-model-o-ambidextro-usb-tipo-a-12000-dpi-negro.jpg'),
+(NULL,'Portatil',300,'https://cdn.grupoelcorteingles.es/SGFM/dctm/MEDIA03/202408/23/00115215599372____6__1200x1200.jpg')
